9020a9d29003e815e81bdf7ba41e720387af55cc
nickvanhooff <nickvanhooff03@gmail.com>
2024-12-09 11:20:29 +0100

    Update MaintenanceListView columns and add AttractionId
    Updated MainWindow.xaml to modify GridView columns in MaintenanceListView:
    - Replaced "Attractie" column with "AttractieId" binding to AttractionId.
    - Replaced "Opmerkingen" column with "Description" binding to Description.
    
    Added AttractionId property of type int to Maintenance class in Maintenance.cs.

--- Models/Maintenance.cs
+++ Models/Maintenance.cs
@@ -9,9 +9,11 @@ namespace ParkbeheerderDashboard.Models
     public class Maintenance
     {
         public int Id { get; set; }
+        public int AttractionId { get; set; }
         public string Description { get; set; }
         public DateTime? Date { get; set; }
         public string Status { get; set; }
+
     }
 
 }
